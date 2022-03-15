daa8608c289b2516982b73eec9539cc8c445bcd9
FernandoDanielZigarra <fernando.zigarra@gmail.com>
2022-03-15 00:55:57 -0300

    ImgAgregada

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3030;
 const path = require('path');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname , 'public')));
 
 app.listen (port , () => {
     console.log(`El servidor esta corriendo en el puerto ${port}`)
@@ -16,12 +16,12 @@ app.get('/babbage' , (req,res) => {
     res.sendFile(path.join(__dirname , './views/babbage.html'))
 });
 
-app.get('/banners-lee' , (req,res) => {
-    res.sendFile(path.join(__dirname , './views/banners-lee.html'))
+app.get('/berners-lee' , (req,res) => {
+    res.sendFile(path.join(__dirname , './views/berners-lee.html'))
 });
 
-app.get('/clake' , (req,res) => {
-    res.sendFile(path.join(__dirname , './views/clake.html'))
+app.get('/clarke' , (req,res) => {
+    res.sendFile(path.join(__dirname , './views/clarke.html'))
 });
 
 app.get('/hamilton' , (req,res) => {
