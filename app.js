78f53375a3589edb9e3fb677f3ff072c4dd59cd1
FernandoDanielZigarra <fernando.zigarra@gmail.com>
2022-03-15 21:33:32 -0300

    StylesAgregados

--- app.js
+++ app.js
@@ -2,41 +2,40 @@ const express = require('express');
 const app = express();
 const port = 3030;
 const path = require('path');
-app.use(express.static(path.join(__dirname , 'public')));
+app.use(express.static('public'));
+
+app.get('/' , (req,res) => {
+    return res.sendFile(path.resolve(__dirname , 'views' , 'index.html'))
+});
+
+app.get('/babbage' , (req,res) => {
+    return res.sendFile(path.resolve(__dirname , 'views' , 'babbage.html'))
+});
+
+app.get('/berners-lee' , (req,res) => {
+    return res.sendFile(path.resolve(__dirname , 'views' , 'berners-lee.html'))
+});
+
+app.get('/clarke' , (req,res) => {
+    return res.sendFile(path.resolve(__dirname , 'views' , 'clarke.html'))
+});
+
+app.get('/hamilton' , (req,res) => {
+    return res.sendFile(path.resolve(__dirname , 'views' , 'hamilton.html'))
+});
+
+app.get('/hopper' , (req,res) => {
+    return res.sendFile(path.resolve(__dirname , 'views' , 'hopper.html'))
+});
+
+app.get('/lovelace' , (req,res) => {
+    return res.sendFile(path.resolve(__dirname , 'views' , 'lovelace.html'))
+});
+
+app.get('/turing' , (req,res) => {
+    return res.sendFile(path.resolve(__dirname , 'views' , 'turing.html'))
+});
 
 app.listen (port , () => {
     console.log(`El servidor esta corriendo en el puerto ${port}`)
 });
-
-app.get('/' , (req,res) => {
-    res.sendFile(path.join(__dirname , './views/index.html'))
-});
-
-app.get('/babbage' , (req,res) => {
-    res.sendFile(path.join(__dirname , './views/babbage.html'))
-});
-
-app.get('/berners-lee' , (req,res) => {
-    res.sendFile(path.join(__dirname , './views/berners-lee.html'))
-});
-
-app.get('/clarke' , (req,res) => {
-    res.sendFile(path.join(__dirname , './views/clarke.html'))
-});
-
-app.get('/hamilton' , (req,res) => {
-    res.sendFile(path.join(__dirname , './views/hamilton.html'))
-});
-
-app.get('/hopper' , (req,res) => {
-    res.sendFile(path.join(__dirname , './views/hopper.html'))
-});
-
-app.get('/lovelace' , (req,res) => {
-    res.sendFile(path.join(__dirname , './views/lovelace.html'))
-});
-
-app.get('/turing' , (req,res) => {
-    res.sendFile(path.join(__dirname , './views/turing.html'))
-});
-
